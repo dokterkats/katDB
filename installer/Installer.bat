3c47f6e4a9274e14225abd505a3c4eb33386abf9
dokterkats <73393296+dokterkats@users.noreply.github.com>
2022-07-03 19:55:20 +0000

    Add files via upload

--- installer/Installer.bat
+++ installer/Installer.bat
@@ -1,4 +1,9 @@
 @echo off
+echo Thanks for using KatDB!
+CALL:ECHORED "This installer will remove all current custom configs and replace them with the KatDB configs"
+echo Kat's pre-set configs will remain in tact.
+echo Please exit the program now if you do not want to remove your current custom configs
+pause
 echo Installing configs from GitHub
 TIMEOUT 5
 wget https://github.com/dokterkats/katDB/archive/refs/heads/main.zip
@@ -21,3 +26,7 @@ rmdir /s katDB-main
 cls
 echo Done
 pause
+
+:ECHORED
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Red %1
+goto:eof
